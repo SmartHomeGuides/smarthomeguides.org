833c917d118c2d4b4732f0728dd0d17d63fa4244
Anthony Da Mota <AkdM@users.noreply.github.com>
2026-03-15 16:35:22 +0100

    Update site URL and base path in Astro configuration until we have the final domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://smarthomeguides.org",
+  site: "https://smarthomeguides.github.io",
+  base: "/smarthomeguides.org",
 
   i18n: {
     defaultLocale: "en",
